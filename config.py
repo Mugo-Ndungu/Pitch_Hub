aa44bb0525ba76bc9fbd62279680bace964e387b
Mugo-Ndungu <twinnymugo@gmail.com>
2019-02-15 17:46:31 +0300

    Second deployment

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:aaronmichelle@localhost/pitches'
-    SQLALCHEMY_TRACk_NOTIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:aaronmichelle@localhost/pitches'
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
     SECRET_KEY = 'os.environ.get("SECRET_KEY")'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     # email configurations
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MAIL_USERNAME='twinnymugo@gmail.com'
 export MAIL_PASSWORD='0720863011'
-SECRET_KEY='Paula1234!'
+export SECRET_KEY='Paula1234!'
+
 python3.6 manage.py server
